05cdac35104f22c9cbe355514e073d2dfa38bb55
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2025-04-30 14:56:08 +0200

    Change a yt compression algorithms
    Make them easier to understand and tweak their params for better tg perf

--- yt_dlp_wrapper.py
+++ yt_dlp_wrapper.py
@@ -1,19 +1,46 @@
+from enum import Enum
 import ffmpeg
 import os
 import yt_dlp
 
-ydl_opt = {
-    "format": "bestvideo[height<=1080][width>1080][vcodec^=avc][ext=mp4]+bestaudio/"
-              "bestvideo[width<=1080][vcodec^=avc][ext=mp4]+bestaudio/"
-              "best[height<=1080][width>1080]/"
-              "best[width<=1080]",
+
+class CompressionLevel(Enum):
+    NO_RESIZE = 0
+    RESIZE_480 = 480
+    RESIZE_360 = 360
+
+
+ydl_options = {
+    "format": "bestvideo[height<=720][width>720][vcodec^=avc][ext=mp4]+bestaudio/"
+              "bestvideo[width<=720][vcodec^=avc][ext=mp4]+bestaudio/"
+              "best[height<=720][width>720]/"
+              "best[width<=720]",
     "merge_output_format": "mp4",
     "outtmpl": "temp/yt-dlp/%(webpage_url_domain)s-%(id)s.%(ext)s",
 }
 
 
+compression_scenarios = [
+    {
+        "name": "hq",
+        "size_threshold": 85 * 1000 * 1000,
+        "resolution_resize": CompressionLevel.NO_RESIZE,
+    },
+    {
+        "name": "mq",
+        "size_threshold": 175 * 1000 * 1000,
+        "resolution_resize": CompressionLevel.RESIZE_480,
+    },
+    {
+        "name": "lq",
+        "size_threshold": 500 * 1000 * 1000,
+        "resolution_resize": CompressionLevel.RESIZE_360,
+    },
+]
+
+
 def download(link):
-    with yt_dlp.YoutubeDL(ydl_opt) as ydl:
+    with yt_dlp.YoutubeDL(ydl_options) as ydl:
         info_dict = ydl.extract_info(link, download=True)
         vid_filename = ydl.prepare_filename(info_dict)
         size = os.path.getsize(vid_filename)
@@ -24,31 +51,21 @@ def download(link):
 
 
 def compress_video(video_path, original_size, is_vertical=False):
-    if original_size < 75 * 1000 * 1000:
-        compressed_video_path = video_path.replace(
-            ".mp4", "-compressed-high.mp4")
-        if not os.path.exists(compressed_video_path):
-            compress_video_hq(video_path, compressed_video_path)
-        compressed_size = os.path.getsize(compressed_video_path)
-        if compressed_size < 50 * 1000 * 1000:
-            return compressed_video_path
-
-    if original_size < 125 * 1000 * 1000:
-        compressed_video_path = video_path.replace(
-            ".mp4", "-compressed-medium.mp4")
-        if not os.path.exists(compressed_video_path):
-            compress_video_mq(video_path, compressed_video_path, is_vertical)
-        compressed_size = os.path.getsize(compressed_video_path)
-        if compressed_size < 50 * 1000 * 1000:
-            return compressed_video_path
-
-    compressed_video_path = video_path.replace(
-        ".mp4", "-compressed-low.mp4")
-    if not os.path.exists(compressed_video_path):
-        compress_video_lq(video_path, compressed_video_path, is_vertical)
-    compressed_size = os.path.getsize(compressed_video_path)
-    if compressed_size < 50 * 1000 * 1000:
-        return compressed_video_path
+    for scenario in compression_scenarios:
+        if original_size < scenario["size_threshold"]:
+            compressed_video_path = video_path.replace(
+                ".mp4", f"-compressed-{scenario['name']}.mp4")
+            if not os.path.exists(compressed_video_path):
+                if scenario["resolution_resize"] == CompressionLevel.NO_RESIZE:
+                    run_compression(video_path, compressed_video_path)
+                else:
+                    max_size = scenario['resolution_resize'].value
+                    scale = f"scale={max_size}:-2" if is_vertical else f"scale=-2:{max_size}"
+                    run_compression_with_resizing(
+                        video_path, compressed_video_path, scale)
+            compressed_size = os.path.getsize(compressed_video_path)
+            if compressed_size < 50 * 1000 * 1000:
+                return compressed_video_path
 
     if original_size < 50 * 1000 * 1000:
         # fallback to original video if compression fails
@@ -57,7 +74,7 @@ def compress_video(video_path, original_size, is_vertical=False):
     raise Exception("Video is too large to compress")
 
 
-def compress_video_hq(video_path, output_file):
+def run_compression(video_path, output_file):
     ffmpeg\
         .input(video_path)\
         .output(output_file,
@@ -70,23 +87,7 @@ def compress_video_hq(video_path, output_file):
         .run()
 
 
-def compress_video_mq(video_path, output_file, is_vertical=False):
-    scale = "scale=720:-2" if is_vertical else "scale=-2:720"
-    ffmpeg\
-        .input(video_path)\
-        .output(output_file,
-                vcodec="libx264",
-                crf=28,
-                preset="slow",
-                acodec="aac",
-                audio_bitrate="128k",
-                vf=scale)\
-        .overwrite_output()\
-        .run()
-
-
-def compress_video_lq(video_path, output_file, is_vertical=False):
-    scale = "scale=480:-2" if is_vertical else "scale=-2:480"
+def run_compression_with_resizing(video_path, output_file, scale):
     ffmpeg\
         .input(video_path)\
         .output(output_file,
